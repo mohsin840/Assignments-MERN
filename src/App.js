b6bffac5e63507db8ff06c694b6e12ffa209d59c
Mohsin Ali Shah <shahhmoh@corp.contoursoftware.net>
2023-04-27 16:59:44 +0500

    Assignmrnt4 updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import "./App.css";
 
 function WeatherDropdown() {
   const [selectedCountry, setSelectedCountry] = useState('');
@@ -27,23 +28,24 @@ function WeatherDropdown() {
   }
 
   return (
-    <div>
-      <h2>Select a country and city to see the current weather:</h2>
-      <div>
-        <label>
+    <div className='layout'>
+    <div className='border'>
+      <h2 className='maind'>Select a country and city to see the current weather:</h2>
+      <div className='maind'>
+        <label className='maind'>
           Country:
-          <select value={selectedCountry} onChange={handleCountryChange}>
-            <option value="">Select a country</option>
+          <select className='pad' value={selectedCountry} onChange={handleCountryChange}>
+            <option className='maind' value="">Select a country</option>
             <option value="Pakistan">Pakistan</option>
             <option value="Afghanistan">Afghanistan</option>
             <option value="India">India</option>
           </select>
         </label>
       </div>
-      <div>
-        <label>
+      <div className='maind'><br/>
+        <label className='maind'>
           City:
-          <select value={selectedCity} onChange={handleCityChange}>
+          <select className='pad' value={selectedCity} onChange={handleCityChange}>
             <option value="">Select a city</option>
             {selectedCountry === 'Pakistan' && (
               <>
@@ -73,7 +75,7 @@ function WeatherDropdown() {
         </label>
       </div>
       {weatherData && (
-        <div>
+        <div className='maind'>
           <h3>Current weather in {selectedCity}, {selectedCountry}:</h3>
           <p>Temperature: {weatherData.main.temp-273}°C</p>
           <p>Weather: {weatherData.weather[0].description}</p>
@@ -83,6 +85,7 @@ function WeatherDropdown() {
       {error && (
         <p>Something went wrong while fetching the weather data. Please try again later.</p>
       )}
+    </div>
     </div>
   );
 }
